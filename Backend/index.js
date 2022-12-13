8bfacbf39ed2322232fafa46eb2c8a280e7caab1
Maria Riosdelgado <mariariosdelgado@Marias-iMac.local>
2022-12-13 13:07:37 -0500

    home page

--- Backend/index.js
+++ Backend/index.js
@@ -4,11 +4,13 @@ const express = require('express')
 const app = express()
 
 //Routes
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/cards', require('./controllers/cards'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('home')
 })
 
 //Wildcard route, must stay at the bottom or it will override
