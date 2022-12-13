e4eb0077d0125355c3df838cfe9c88f1018c6e8b
Maria Riosdelgado <mariariosdelgado@Marias-iMac.local>
2022-12-13 11:37:56 -0500

    added 404 page

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+//Dependencies
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Routes
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(3000)
+//Wildcard route, must stay at the bottom or it will override
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
